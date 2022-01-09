022110bd6d434065d0f3ab2f3c614244b915c1d9
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-01-09 14:56:38 +0600

    Генерация основных компонентов, стилизация компонента header. Настройка перехода по компонентам

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
+import { UserItemComponent } from './users/user-item/user-item.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersComponent,
+    UserItemComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
